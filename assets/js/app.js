73cacb98ca0658e8ec7a8963b1a1bf5ea6000691
Ajay <ajay.3mindsdigital@gmail.com>
2024-09-13 01:50:09 +0530

    Added responsive for homepage

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,35 +1,10 @@
 $(document).ready(() => {
-	// Load more Functionality
-	$(".projects__single").slice(0, 2).css("display", "flex").show()
-	let isExpanded = false
-
-	$(".projects__button").click((e) => {
-		e.preventDefault()
-
-		isExpanded
-			? $(".projects__single:visible")
-					.slice(-3)
-					.slideUp("slow", () => {
-						$(this).css("display", "none")
-					})
-			: $(".projects__single:hidden")
-					.slice(0, 3)
-					.css("display", "flex")
-					.slideDown("slow")
-
-		$(".projects__buttonArrow").css(
-			"rotate",
-			isExpanded ? "0deg" : "180deg"
-		)
-		isExpanded = !isExpanded
-	})
-
 	// Sliders
 	const clientsSliderOptionsPara = {
 		// Optional parameters
 		loop: true,
 		slidesPerView: "auto",
-		speed: 4000,
+		speed: 2000,
 		grabCursor: false,
 		mousewheelControl: false,
 		keyboardControl: true,
@@ -53,6 +28,7 @@ $(document).ready(() => {
 
 	const feedbackRibbonSlider = new Swiper(".feedback__ribbon", {
 		...clientsSliderOptionsPara,
+		speed: 4000,
 		autoplay: {
 			delay: 0,
 			disableOnInteraction: true,
@@ -65,19 +41,15 @@ $(document).ready(() => {
 		loop: true,
 		on: {
 			slideChangeTransitionStart: () => {
-				// Remove rotation classes from all slides
 				$(".swiper-slide").removeClass(
 					"swiper-slide-rotate-next swiper-slide-rotate-prev"
 				)
 
-				// Check the direction of the slide change
 				if (feedbackSlider.swipeDirection === "next") {
-					// Add clockwise rotation for the next slide
 					$(
 						feedbackSlider.slides[feedbackSlider.activeIndex]
 					).addClass("swiper-slide-rotate-next")
 				} else if (feedbackSlider.swipeDirection === "prev") {
-					// Add counterclockwise rotation for the previous slide
 					$(
 						feedbackSlider.slides[feedbackSlider.activeIndex]
 					).addClass("swiper-slide-rotate-prev")
