5fc55fda6d9ba1b9faa2624ab2fa9148e513747f
Rohit Bhirud <luffy@Rohits-MacBook-Pro.local>
2024-10-03 01:50:17 +0530

    footer fix

--- assets/js/app.js
+++ assets/js/app.js
@@ -845,17 +845,14 @@ $(document).ready(function () {
 				duration: 0.5,
 			},
 			"-=0.1"
-		)
-		.from(
-			".footer__bottomLinks li",
-			{
-				y: 20,
-				opacity: 0,
-				stagger: 0.1,
-				duration: 0.5,
-			},
-			"-=1"
 		);
+
+	gsap.from(".footer__bottomLinks li", {
+		y: 20,
+		opacity: 0,
+		stagger: 0.1,
+		duration: 0.5,
+	});
 
 	gsap.from(".footer__copyRights", {
 		y: 20,
